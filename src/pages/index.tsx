5a706ecc3c015c109c90763a726e8cb8f3427664
luhoro <luisashorowitz@gmail.com>
2023-09-21 21:56:23 -0300

    Feat: Main home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,61 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+
 export default function Home() {
   return (
-    <> 
-      <h1>Skill Up</h1>
+    <>
+      <Head>
+        <title>Skill Up - Home</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <section className={styles.content}>
+            <h2>Descubra uma melhor versão de si mesmo</h2>
+
+            <span>
+              Conheça a plataforma perfeita para descobrir e aprimorar suas
+              habilidades profissionais e conseguir crescer em sua carreira.
+            </span>
+
+            <div>
+              <button>ACESSE AGORA</button>
+            </div>
+          </section>
+
+          <img src="/images/banner-conteudos.png" alt="Conteúdos Skill Up" />
+        </div>
+
+        <div className={styles.divisor}></div>
+
+        <div className={styles.sectionContent}>
+          <section>
+            <h2>Aprenda tudo sobre o mundo dev</h2>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+              numquam accusamus cupiditate adipisci nostrum? Ea sapiente non
+              saepe eius sequi.
+            </span>
+          </section>
+
+          <img src="/images/financasApp.png" alt="Conteúdos da plataforma" />
+        </div>
+
+        <div className={styles.divisor}></div>
+
+        <div className={styles.sectionContent}>
+          <img src="/images/webDev.png" alt="Conteúdos da plataforma" />
+
+          <section>
+            <h2>Aprenda tudo sobre o mundo dev</h2>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+              numquam accusamus cupiditate adipisci nostrum? Ea sapiente non
+              saepe eius sequi.
+            </span>
+          </section>
+        </div>
+      </main>
     </>
   )
 }
