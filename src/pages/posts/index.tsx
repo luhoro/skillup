b41bfa17db8d7f16e6822665c13692a0509ea6af
luhoro <luisashorowitz@gmail.com>
2023-09-27 22:01:25 -0300

    Build: CMS priomic.io integration

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
-import styles from './posts.module.scss'
-import Image from 'next/image'
+import styles from './Posts.module.scss'
+import {FiChevronsLeft, FiChevronLeft, FiChevronRight, FiChevronsRight} from 'react-icons/fi'
 
-const Posts = () => {
+export default function Posts ()  {
   return (
     <>
       <Head>
@@ -28,10 +28,30 @@ const Posts = () => {
             <time>26 de Setembro 2023</time>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae voluptas rem eius, nobis minus cum et quis eaque! Culpa, officia?</p>
           </Link>
+
+          <div className={styles.buttonNavigate}>
+            <div>
+              <button>
+                <FiChevronsLeft size={30} color='var(--gray-700)' />
+              </button>
+
+              <button>
+                <FiChevronLeft size={30} color='var(--gray-900)' />
+              </button>
+            </div>
+
+            <div>
+              <button>
+                <FiChevronRight size={30} color='var(--gray-900)' />
+              </button>
+
+              <button>
+                <FiChevronsRight size={30} color='var(--gray-700)' />
+              </button>
+            </div>
+          </div>
         </div>
       </main>
     </>
   )
 }
-
-export default Posts
