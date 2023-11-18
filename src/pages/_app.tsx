b2ee339daf7c3014625be8d4260c3bcd9a3f07f0
luhoro <luisashorowitz@gmail.com>
2023-11-17 22:32:28 -0300

    Fix: Google Fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
-import Header from '../components/Header'
-import type { AppProps } from 'next/app'
+import Header from "../components/Header";
+import type { AppProps } from "next/app";
 
-import '../styles/global.scss'
-import Head from 'next/head'
-import Footer from '../components/Footer'
+import "../styles/global.scss";
+import Head from "next/head";
+import Footer from "../components/Footer";
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,13 +11,19 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Skill Up</title>
         <link rel="shortcut icon" href="favicon.svg" type="image/x-icon" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      
+
       <Header />
       <Component {...pageProps} />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
